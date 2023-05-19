e63211c9dc1d2a537d9c877e58512567141388fb
Isachenko Ivan <vanisachenko@gmail.com>
2023-05-19 18:40:03 +0600

    Cors policy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 
 
 
-app.use(cors({credentials: true, origin: ['http://127.0.0.1/5173', 'http://localhost:5173']}))
+app.use(cors({credentials: true, origin: ['http://127.0.0.1:5173', 'http://localhost:5173']}))
 app.use(express.json())
 
 app.use('/users', UserRouter)
