9139a4d8287935b4d573f356a8eff1b4c03cb607
Isachenko Ivan <vanisachenko@gmail.com>
2023-05-23 21:15:59 +0600

    Production DB

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const db = require('mongoose')
 
 module.exports = async function() {
-    await db.connect("mongodb://185.177.219.117:27017/LCT", {
+    await db.connect(process.env.DB_URL_NONLOCAL, {
         authSource: "admin",
-        user: "self",
+        user: "root",
         pass: "admin",
         useNewUrlParser: true,
         useUnifiedTopology: true,
